3788718216013b5b421bb967b6bbc85dfb236423
Jujhar Singh <jujhar@jujhar.com>
2021-07-27 16:01:20 +0100

    cleans up page a bit

--- src/index.js
+++ src/index.js
@@ -12,15 +12,18 @@ const template = Mustache.render(
 <div class="col-lg-6 col-xxl-4 mb-5">
   <div class="card bg-light border-0 h-100">
       <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-          <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+      <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
           <h2 class="fs-4 fw-bold">{{id}}. {{title}}</h2>
           <p class="mb-0" class="card-text">
             {{description}}
           </p>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item high-score">{{high_score}}</li>
-            <li class="list-group-item low-score">{{low_score}}</li>
-          </ul>
+          <a class="btn btn-primary btn-sm" href="#!">Show</a>
+          <div class="card-body high-score">
+            {{high_score}}
+          </div>
+          <div class="card-body low-score">
+            {{low_score}}
+          </div>
           {{#more_info_link}}
           <div class="card-body">
             <a href="{{more_info_link}}" class="card-link">More Info</a>
