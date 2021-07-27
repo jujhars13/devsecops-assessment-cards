1f7645f1adf18dfecbcadb98b0d96e737ea3b7a0
Jujhar Singh <jujhar@jujhar.com>
2021-07-27 14:27:53 +0100

    cleans up page a bit

--- src/index.js
+++ src/index.js
@@ -5,41 +5,35 @@ const cardContainer = document.getElementById("card-container");
 //document.getElementById("debug").innerHTML = JSON.stringify(data, null, 2);
 
 // render the cards
-const template = Mustache.render(`
+const template = Mustache.render(
+  `
 {{#data}}
-<div class="category" data-category="{{categoryName}}">
-  <h2>{{categoryName}}</h2>
-  <div class="row">
-    {{#cards}}
-    <div class="col">
-      <div class="card" style="width: 18rem;" data-id="{{id}}">
-        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect>
-        <text x="20%" y="50%" fill="#dee2e6" dy=".3em">{{id}}. {{title}}</text></svg>
-          <rect width="100%" height="100%" fill="#868e96"></rect>
-        </svg>
-        <div class="card-header">
-          {{id}}. {{title}}
-        </div>
-        <div class="card-body">
-          <p class="card-text">
+{{#cards}}
+<div class="col-lg-6 col-xxl-4 mb-5">
+  <div class="card bg-light border-0 h-100">
+      <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+          <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+          <h2 class="fs-4 fw-bold">{{id}}. {{title}}</h2>
+          <p class="mb-0" class="card-text">
             {{description}}
           </p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item high-score">{{high_score}}</li>
-          <li class="list-group-item low-score">{{low_score}}</li>
-        </ul>
-        {{#more_info_link}}
-        <div class="card-body">
-          <a href="{{more_info_link}}" class="card-link">More Info</a>
-        </div>
-        {{/more_info_link}}
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item high-score">{{high_score}}</li>
+            <li class="list-group-item low-score">{{low_score}}</li>
+          </ul>
+          {{#more_info_link}}
+          <div class="card-body">
+            <a href="{{more_info_link}}" class="card-link">More Info</a>
+          </div>
+          {{/more_info_link}}
+          <footer class="card-footer">{{categoryName}}</footer>
       </div>
-    </div>
-    {{/cards}}
   </div>
 </div>
+{{/cards}}
 {{/data}}
-`, { data });
+`,
+  { data }
+);
 
 cardContainer.innerHTML = template;
