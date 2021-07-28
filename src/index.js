956941458d87e6af2b15a2d054163d6ee25ac1af
Jujhar Singh <jujhar@jujhar.com>
2021-07-28 09:26:48 +0100

    basic cards in place

--- src/index.js
+++ src/index.js
@@ -4,30 +4,24 @@ import data from "./js/cardModel.js";
 const cardContainer = document.getElementById("card-container");
 //document.getElementById("debug").innerHTML = JSON.stringify(data, null, 2);
 
+
 const cardTemplate = Mustache.render(
   `
 {{#data}}
 {{#cards}}
 <div class="main-card-container">
-        <div class="question-card" >
-          <div class="card-front">
+        <div class="question-card">
+          <div class="card-front {{{categoryName}}}">
             <h1>{{id}}. {{title}}</h1>
-            <p>
-            {{description}}
+            <p>{{description}}</p>
             <footer class="card-footer">{{categoryName}}</footer>
-          </p>
           </div>
           <div class="card-back">
-            <p class="high-score">
-              {{high_score}}
-            </p>
-            <p class="low-score">
-              {{low_score}}
-            </p>
+            <h1>{{id}}. {{title}}</h1>
+            <p class="high-score">{{high_score}}</p>
+            <p class="low-score">{{low_score}}</p>
             {{#more_info_link}}
-            <p >
-              <a href="{{more_info_link}}" class="card-link">More Info</a>
-            </p>
+            <p><a href="{{more_info_link}}" class="card-link">More Info</a></p>
             {{/more_info_link}}
           </div>
         </div>
