fc95e1b17dcf5598d94474dea31693f5ad16307d
Jujhar Singh <jujhar@jujhar.com>
2021-07-28 09:03:31 +0100

    basic animations in place

--- src/index.js
+++ src/index.js
@@ -4,43 +4,34 @@ import data from "./js/cardModel.js";
 const cardContainer = document.getElementById("card-container");
 //document.getElementById("debug").innerHTML = JSON.stringify(data, null, 2);
 
-const cardTemplate = Mustache.render(`
-{{#data}}
-{{#cards}}
-
-{{/cards}}
-{{/data}}
-`, { data });
-
-// render the cards
-const classicTemplate = Mustache.render(
+const cardTemplate = Mustache.render(
   `
 {{#data}}
 {{#cards}}
-<div class="col-lg-6 col-xxl-4 mb-5">
-  <div class="card bg-light border-0 h-100">
-      <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-      <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
-          <h2 class="fs-4 fw-bold">{{id}}. {{title}}</h2>
-          <p class="mb-0" class="card-text">
+<div class="main-card-container">
+        <div class="question-card" >
+          <div class="card-front">
+            <h1>{{id}}. {{title}}</h1>
+            <p>
             {{description}}
+            <footer class="card-footer">{{categoryName}}</footer>
           </p>
-          <a class="btn btn-primary btn-sm" href="#!">Show</a>
-          <div class="card-body high-score">
-            {{high_score}}
           </div>
-          <div class="card-body low-score">
-            {{low_score}}
+          <div class="card-back">
+            <p class="high-score">
+              {{high_score}}
+            </p>
+            <p class="low-score">
+              {{low_score}}
+            </p>
+            {{#more_info_link}}
+            <p >
+              <a href="{{more_info_link}}" class="card-link">More Info</a>
+            </p>
+            {{/more_info_link}}
           </div>
-          {{#more_info_link}}
-          <div class="card-body">
-            <a href="{{more_info_link}}" class="card-link">More Info</a>
-          </div>
-          {{/more_info_link}}
-          <footer class="card-footer">{{categoryName}}</footer>
+        </div>
       </div>
-  </div>
-</div>
 {{/cards}}
 {{/data}}
 `,
@@ -51,7 +42,7 @@ cardContainer.innerHTML = cardTemplate;
 
 // add flip animation to cards
 const cards = document.getElementsByClassName("question-card");
-Object.values(cards).forEach(el => {
+Object.values(cards).forEach((el) => {
   el.addEventListener("click", () => {
     el.classList.toggle("flipCard");
   });
