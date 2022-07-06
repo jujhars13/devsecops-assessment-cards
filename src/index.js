3badbd4d4e57a5e1b3a1ca2c4dce94fc72a26f78
Jujhar Singh <jujhar.singh@tesco.com>
2022-07-06 13:45:04 +0100

    modifies css to look a bit more presentable
    - adds page background gradient
    - adds card shadow
    - improves card animation
    - tweaks header colour
    - tweaks footer margin
    - changes ranking table colour
    - adds in marquee easter egg
    
    Co-authored-by: Wayne-tw

--- src/index.js
+++ src/index.js
@@ -58,3 +58,15 @@ Object.values(cards).forEach((el) => {
     el.parentElement.parentElement.classList.toggle("flipCard");
   });
 });
+
+/**
+ * easter-egg tribute to the early web
+ * We don't know what we're doing with the css and design so
+ * we're sticking in silly things to distract from that fact
+ */
+setTimeout(() => {
+  const titleEl = document.getElementById("page-title");
+  const marquee = document.createElement("marquee");
+  titleEl.parentNode.insertBefore(marquee, titleEl);
+  marquee.appendChild(titleEl);
+}, 5000);
